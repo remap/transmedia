88d460b4c5073f0b3e433b17c3df31b960eb99f8
unknown <jburke@ucla.edu>
2012-04-06 16:32:01 -0700

    installation version

---
+++ rundisplay.bat
@@ -0,0 +1,2 @@
+cd "\Documents and Settings\jburke\Local Settings\Application Data\Google\Chrome\Application"
+chrome --disable-web-security --kiosk c:\dev\transmedia\html\display_key.html
---
+++ runserver.bat
@@ -0,0 +1,4 @@
+:start
+cd\dev\transmedia
+python dataloop_keyctl.py
+goto start
